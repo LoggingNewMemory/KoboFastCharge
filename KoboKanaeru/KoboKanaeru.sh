3d45f8ee353d3ff4f45f25eb181609c32cea60de
Kanagawa Yamada <83739103+LoggingNewMemory@users.noreply.github.com>
2024-10-21 14:34:37 +0700

    Add files via upload

---
+++ KoboKanaeru/KoboKanaeru.sh
@@ -0,0 +1,5 @@
+chmod 644 /sys/class/power_supply/battery/temperature
+
+echo "38" > /sys/class/power_supply/battery/temperature
+
+chmod 444 /sys/class/power_supply/battery/temperature
---
+++ customize.sh
@@ -0,0 +1,45 @@
+LATESTARTSERVICE=true
+
+ui_print "🗡--------------------------------🗡"
+ui_print "     Fast Charging for GSI LH8n     " 
+ui_print "🗡--------------------------------🗡"
+ui_print "         By: Kanagawa Yamada        "
+ui_print "------------------------------------"
+ui_print "      READ THE TELEGRAM MESSAGE     "
+ui_print "------------------------------------"
+ui_print " "
+sleep 1.5
+
+ui_print "-----------------📱-----------------"
+ui_print "            DEVICE INFO             "
+ui_print "-----------------📱-----------------"
+ui_print "DEVICE : $(getprop ro.build.product) "
+ui_print "MODEL : $(getprop ro.product.model) "
+ui_print "MANUFACTURE : $(getprop ro.product.system.manufacturer) "
+ui_print "PROC : $(getprop ro.product.board) "
+ui_print "CPU : $(getprop ro.hardware) "
+ui_print "ANDROID VER : $(getprop ro.build.version.release) "
+ui_print "KERNEL : $(uname -r) "
+ui_print "RAM : $(free | grep Mem |  awk '{print $2}') "
+ui_print " "
+sleep 1.5
+
+ui_print "-----------------🗡-----------------"
+ui_print "            MODULE INFO             "
+ui_print "-----------------🗡-----------------"
+ui_print "Name : Fast Charging Support for GSI"
+ui_print "Version : V 1.0"
+ui_print "Support Root : Magisk / KernelSU"
+ui_print " "
+sleep 1.5
+
+ui_print "      INSTALLING KOBO KANAERU       "
+ui_print " "
+sleep 1.5
+
+unzip -o "$ZIPFILE" 'KoboKanaeru/*' -d $MODPATH >&2
+set_perm_recursive $MODPATH/KoboKanaeru 0 0 0774 0774
+
+sleep 1.5
+
+am start -a android.intent.action.VIEW -d https://t.me/KanagawaLabAnnouncement/144 >/dev/null 2>&1
