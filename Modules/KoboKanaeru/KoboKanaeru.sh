068815e828ab3240477c64bf2206115a83c69784
KanagawaYamada <yamadakernel.project@gmail.com>
2025-12-18 12:16:33 +0700

    Gtw worok apa gak tapi ini keknya
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/KoboKanaeru/KoboKanaeru.sh
+++ Modules/KoboKanaeru/KoboKanaeru.sh
@@ -1,77 +1,84 @@
-#!/bin/bash
+#!/system/bin/sh
 
-# Kobo Fast Charge, collab with Charging Enhancer+ By: VelocityFox22
+DEFAULT_CURRENT=3000000
+DEFAULT_VOLTAGE=5000000
 
-tweak() {
-	if [ -f $2 ]; then
-		chmod 644 $2 >/dev/null 2>&1
-		echo $1 >$2 2>/dev/null
-		chmod 444 $2 >/dev/null 2>&1
-	fi
+get_max_values() {
+    if [ -f "/sys/class/power_supply/battery/voltage_max_design" ]; then
+        MAX_VOLTAGE=$(cat /sys/class/power_supply/battery/voltage_max_design)
+    else
+        MAX_VOLTAGE=$DEFAULT_VOLTAGE
+    fi
+
+    if [ -f "/sys/class/power_supply/battery/charge_current_max" ]; then
+         MAX_CURRENT=$(cat /sys/class/power_supply/battery/charge_current_max)
+    else
+         MAX_CURRENT=$DEFAULT_CURRENT
+    fi
+    
+    if [ "$MAX_CURRENT" -lt 2000000 ]; then
+        MAX_CURRENT=3000000 
+    fi
+
+    if [ "$MAX_VOLTAGE" -gt 5500000 ]; then
+        MAX_CURRENT=9000000
+    fi
 }
 
-# Disable thermal zones for maximum performance
+lock_val() {
+    if [ -f "$2" ]; then
+        chmod 0666 "$2" >/dev/null 2>&1
+        echo "$1" > "$2" 2>/dev/null
+        chmod 0444 "$2" >/dev/null 2>&1
+    fi
+}
+
+get_max_values
+
 for zone in /sys/class/thermal/thermal_zone*/mode; do
-    tweak "$zone" "disabled"
+    lock_val "disabled" "$zone"
 done
 
-# BMS (Battery Management System) temperature tweaks
-for bms in /sys/devices/platform/bms/*; do
-    tweak 150 $bms/temp_cool
-    tweak 460 $bms/temp_hot
-    tweak 460 $bms/temp_warm
-done 
-
-# Platform charger optimizations
-for platformCharger in /sys/devices/platform/charger/*; do
-    tweak 9000000 $platformCharger/current_max
-    tweak 9000 $platformCharger/sc_ibat_limit
-    tweak 14000 $platformCharger/sc_stime
-    tweak 9000000 $platformCharger/hw_current_max
-    tweak 9000000 $platformCharger/pd_current_max
-    tweak 9000000 $platformCharger/ctm_current_max
-    tweak 9000000 $platformCharger/sdp_current_max
+find /sys/ -name "temp_cool" -o -name "temp_hot" -o -name "temp_warm" 2>/dev/null | while read path; do
+    case "$path" in
+        *"cool"*) lock_val "150" "$path" ;;
+        *"hot"*)  lock_val "480"  "$path" ;;
+        *"warm"*) lock_val "480" "$path" ;;
+    esac
 done
 
-# Platform main charging parameters
-for platformMain in /sys/devices/platform/main/*; do
-    tweak 9000000 $platformMain/current_max
-    tweak 9000000 $platformMain/constant_charge_current_max
+for feature in fast_charge force_fast_charge boost_mode turbo_mode pd_allowed allow_hvdcp3; do
+    find /sys/class/power_supply/ -name "$feature" 2>/dev/null | while read path; do
+        lock_val "1" "$path"
+    done
 done
 
-# Platform battery parameters
-for platformBattery in /sys/devices/platform/battery/*; do
-    tweak 9000000 $platformBattery/current_max
-    tweak 9000000 $platformBattery/constant_charge_current_max
+for limit in current_max constant_charge_current_max input_current_limit hw_current_max pd_current_max ctm_current_max sdp_current_max restricted_current; do
+    find /sys/class/power_supply/ -name "$limit" 2>/dev/null | while read path; do
+        lock_val "$MAX_CURRENT" "$path"
+    done
 done
 
-# Power supply current limits (original KoboKanaeru values)
-for powerSupplyCurrent in /sys/class/power_supply/*; do
-    tweak 5750000 $powerSupplyCurrent/constant_charge_current_max
-    tweak 12500000 $powerSupplyCurrent/input_current_limit
+find /sys/class/power_supply/ -name "input_voltage_limit" 2>/dev/null | while read path; do
+        lock_val "$MAX_VOLTAGE" "$path"
 done
 
-# Enhanced power supply optimizations with higher limits
-for path in $(find /sys/class/power_supply -type f \( -name "constant_charge_current_max" -o -name "input_current_limit" \)); do
-    tweak "$path" 3000000
+if [ -d "/sys/devices/mtk-battery" ]; then
+    lock_val "$MAX_CURRENT" "/sys/devices/mtk-battery/restricted_current"
+    lock_val "$MAX_CURRENT" "/sys/devices/mtk-battery/fc_current_limit"
+fi
+
+if [ -d "/sys/class/qcom-battery" ]; then
+    lock_val "0" "/sys/class/qcom-battery/restricted_charging"
+    lock_val "0" "/sys/class/qcom-battery/restrict_chg"
+    lock_val "$MAX_CURRENT" "/sys/class/qcom-battery/restricted_current"
+    lock_val "$MAX_CURRENT" "/sys/class/qcom-battery/restrict_cur" 
+fi
+
+find /sys/devices/platform/ -name "constant_charge_current_max" -o -name "input_current_limit" 2>/dev/null | while read path; do
+    lock_val "$MAX_CURRENT" "$path"
 done
 
-# Voltage limits
-tweak 12500000 /sys/class/power_supply/*/input_voltage_limit
-for path in $(find /sys/class/power_supply -type f -name "input_voltage_limit"); do
-    tweak "$path" 5000000
-done
-
-# Additional power supply tweaks
-tweak 100 /sys/class/power_supply/*/siop_level
-tweak 500 /sys/class/power_supply/*/temp_warm
-
-# Enable fast charging features
-for feature in fast_charge boost_mode turbo_mode; do
-    tweak "/sys/class/power_supply/battery/$feature" 1
-done
-
-# MTK and platform specific optimizations
-tweak 10800000 /sys/devices/mtk-battery/restricted_current
-tweak 9000000 /sys/devices/platform/pc_port/current_max
-tweak 9000000 /sys/devices/platform/constant_charge_current__max
+lock_val "0" "/sys/class/power_supply/battery/input_current_limited"
+lock_val "1" "/sys/class/power_supply/battery/input_current_settled"
+lock_val "100" "/sys/class/power_supply/battery/siop_level"
